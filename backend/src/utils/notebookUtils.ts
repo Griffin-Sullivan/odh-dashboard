98c61e0004baec0b28f328135a036eef95374a96
Jan Stourac <jstourac@redhat.com>
2025-10-10 08:39:11 +0200

    [feat] update Notebook annotations - don't pass the logout url annotation
    With the move to the Gateway API, we decided to use single domain name
    for now. This means same domain for all Notebook instances and as such,
    SSO works for all of them including the Dashboard. We decided to remove
    the Log out functionality from the Notebook instances. This decision
    may be revisited in the future if we decide to move to the multiple
    domain approach.
    
    For this change, we stop passing down the relevant annotation with the
    URL used for the logout operation.
    
    We also updated the arguments provided for the Jupyterlab app in the
    workbench removing the `--ServerApp.tornado_settings` parameter. This
    change will remove the presence of the `File -> Log Out` and `File ->
    Shut Down` options from the Jupyterlab menu.

--- backend/src/utils/notebookUtils.ts
+++ backend/src/utils/notebookUtils.ts
@@ -218,7 +218,6 @@ export const assembleNotebook = async (
         'opendatahub.io/dashboard': 'true',
       },
       annotations: {
-        'notebooks.opendatahub.io/oauth-logout-url': `${url}/notebook-controller/${translatedUsername}/home`,
         'notebooks.opendatahub.io/last-size-selection': lastSizeSelection,
         'notebooks.opendatahub.io/last-image-selection': imageSelection,
         'opendatahub.io/username': username,
@@ -249,8 +248,7 @@ export const assembleNotebook = async (
                   --ServerApp.token=''
                   --ServerApp.password=''
                   --ServerApp.base_url=/notebook/${namespace}/${name}
-                  --ServerApp.quit_button=False
-                  --ServerApp.tornado_settings={"user":"${translatedUsername}","hub_host":"${url}","hub_prefix":"/notebook-controller/${translatedUsername}"}`,
+                  --ServerApp.quit_button=False`,
                 },
                 {
                   name: 'JUPYTER_IMAGE',
